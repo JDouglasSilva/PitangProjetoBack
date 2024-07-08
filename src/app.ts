d3c490694aafff607bbc0f5f373f821406465d45
Dag <jddouglassilva@gmail.com>
2024-07-08 17:18:42 -0300

    Continuação da modularização, separando index.ts em app.ts e server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-import { PrismaClient } from '@prisma/client';
 import agendamentoRoutes from './routes/agendamentoRoutes';
 import errorMiddleware from './middlewares/errorMiddleware';
 
 const app = express();
-const port = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +13,4 @@ app.use('/agendamentos', agendamentoRoutes);
 
 app.use(errorMiddleware);
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+export default app;
