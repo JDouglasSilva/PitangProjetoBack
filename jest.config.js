9d8ccc900d9f8e203b09af39853f3e797022591e
Dag <jddouglassilva@gmail.com>
2024-07-15 19:34:36 -0300

    Inplementação do nescessario para testes no backend

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverage: true, 
+  collectCoverageFrom: [
+    'src/**/*.ts', 
+    '!src/**/*.d.ts', 
+  ],
+  coverageDirectory: 'coverage', 
+  coverageReporters: ['text', 'lcov'], 
+};
