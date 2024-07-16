e6b6f96a9d11cac6e98ff9ffe1998ce780dbdb20
Dag <jddouglassilva@gmail.com>
2024-07-16 16:09:46 -0300

    Atualização no teste de cadastro de agendamento, no lugar de usar uma data fixa, usar uma data futura baseado na data atual, para garantir que o teste vai funcionar no futuro

--- tests/agendamentoController.test.ts
+++ tests/agendamentoController.test.ts
@@ -5,10 +5,17 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 let agendamentoId: number | null = null;
 
+const generateFutureDate = (): string => {
+  const date = new Date();
+  date.setFullYear(date.getFullYear() + 10);
+  date.setHours(12, 0, 0, 0);
+  return date.toISOString();
+};
+
 describe('Agendamento Controller', () => {
   beforeAll(async () => {
     // A linha a seguir apaga os dados do banco de dados, usar com cuidado
-    //await prisma.agendamento.deleteMany();
+    await prisma.agendamento.deleteMany();
   });
 
   afterAll(async () => {
@@ -30,7 +37,7 @@ describe('Agendamento Controller', () => {
       .send({
         nomeDoPaciente: 'John Doe',
         dataNascimentoPaciente: '1990-01-01T00:00:00.000Z',
-        dataHoraAgendamento: '2029-07-20T10:00:00.000Z'
+        dataHoraAgendamento: generateFutureDate()
       });
 
     expect(response.status).toBe(201);
@@ -58,7 +65,7 @@ describe('Agendamento Controller', () => {
       .send({
         nomeDoPaciente: 'Jane Doe',
         dataNascimentoPaciente: '1990-01-01T00:00:00.000Z',
-        dataHoraAgendamento: '2029-07-21T10:00:00.000Z'
+        dataHoraAgendamento: generateFutureDate()
       });
 
     const response = await request(app)
